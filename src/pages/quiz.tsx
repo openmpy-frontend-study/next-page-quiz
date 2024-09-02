298b396422179664842235a8d198953f3bece593
openmpy <suhwan@kakao.com>
2024-09-02 19:10:56 +0900

    chore: Update quiz page with saved answers functionality

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { TSavedAnswer } from "@/types/quiz";
+import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -6,6 +8,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export default function Quiz() {
   const [pageIndex, setPageIndex] = useState(0);
   const { data, error } = useSWR(`/api/quiz/${pageIndex}`, fetcher);
+  const [answered, setAnswered] = useState<TSavedAnswer>({});
 
   if (error) {
     return (
@@ -25,5 +28,63 @@ export default function Quiz() {
 
   const { quiz, next, prev, page, total } = data;
 
-  return <div>Quiz</div>;
+  const addAnswer = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const latestAnswers = { ...answered, [name]: value };
+
+    setAnswered(latestAnswers);
+    localStorage.setItem("answers", JSON.stringify(latestAnswers));
+  };
+
+  return (
+    <>
+      <div>
+        <p>
+          {parseInt(page) + 1} / {total}
+        </p>
+      </div>
+      <div>
+        <div>
+          <p>{quiz.question}</p>
+        </div>
+        <ul>
+          {quiz.options.map((option: string, i: number) => (
+            <li key={i}>
+              <input
+                type="radio"
+                name={quiz.id.toString()}
+                onChange={(e) => addAnswer(e)}
+                value={option}
+                checked={answered[quiz.id] === option}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {prev ? (
+          <button onClick={() => setPageIndex(pageIndex - 1)}>이전</button>
+        ) : (
+          <Link href="/"></Link>
+        )}
+        {next ? (
+          <button
+            onClick={() => setPageIndex(pageIndex + 1)}
+            disabled={answered[quiz.id] === undefined}
+            className={
+              answered[quiz.id] === undefined ? "disabledBtn" : "activeBtn"
+            }
+          >
+            다음 문제
+          </button>
+        ) : (
+          answered[quiz.id] !== undefined && (
+            <Link href="/result">
+              <button>끝</button>
+            </Link>
+          )
+        )}
+      </div>
+    </>
+  );
 }
